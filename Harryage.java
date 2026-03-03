8194dda736158d6958c3e7f277cabc25a4e83c34
Kjnightryder <75712428+Kjnightryder@users.noreply.github.com>
2026-03-03 22:56:36 +0530

    Add files via upload

---
+++ Harryage.java
@@ -0,0 +1,10 @@
+public class level1
+{
+    public static void main(String [] args)
+    {
+            //1
+            int age=2024-2000;
+            System.out.println("Harry's age in 2024 is "+age);
+            
+    }
+}
---
+++ pen.java
@@ -0,0 +1,9 @@
+public class level1
+{
+    public static void main(String [] args)
+    {
+            //5
+            int pen=14;
+            System.out.println("number of  pen each student will get "+pen/3+" remaining pen are "+pen%3);
+    }
+}
